8d9b4ab66e43e77519e079a4050c666704440002
user_acronus <user_acronus@TAT0119643W10-1.senacsp.edu.br>
2017-08-02 18:34:57 -0300

    Portifolio do modolo de progamação orientada a objeto java

---
+++ src/br/com/senac/tabu/cltabu.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.senac.tabu;
+
+/**
+ *
+ * @author Aldivan Oliveira
+ */
+public class cltabu {
+    public static void main(String[] args) {
+	tabua cltabu = new tabua();
+	cltabu.setVisible(true);
+    }
+    
+}
